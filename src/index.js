175f77e85cf4038f69d6cc9614ca4b688404216d
Irina <108676588+IrinaOsp@users.noreply.github.com>
2024-02-02 13:10:05 +0100

    refactor: move code to classes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-import { startProject } from "./modules/fileManager.js";
+import FileManager from "./modules/fileManager.js";
 
-await startProject();
+const name = "--username";
+const args = process.argv.slice(2);
+const isUserNameProvided = args.some((arg) => arg.includes(name));
+const userName = isUserNameProvided
+  ? args.find((arg) => arg.includes(name)).slice(name.length + 1)
+  : "Anonymous";
+
+const fileManager = new FileManager(userName);
+
+fileManager.startProject();
--- src/modules/fileManager.js
+++ src/modules/fileManager.js
@@ -1,23 +1,17 @@
-import path from "path";
-import { fileURLToPath } from "url";
 import os from "os";
 
-const name = "--username";
+export default class FileManager {
+  constructor(username) {
+    this.username = username;
+    this.currentPath = os.homedir();
+  }
 
-const getCurrentPath = () => {
-  const __dirname = path.dirname(fileURLToPath(import.meta.url));
-  console.log(`You are currently in ${__dirname}`);
-};
+  startProject() {
+    console.log(`Welcome to the File Manager, ${this.username}!`);
+    this.printCurrentPath();
+  }
 
-export const startProject = async () => {
-  const args = process.argv.slice(2);
-  const userName = args
-    .find((arg) => arg.includes(name))
-    .slice(name.length + 1);
-  console.log(`Welcome to the File Manager, ${userName}!`);
-  const getStartingPath = () => {
-    const homePath = os.homedir();
-    console.log(`You are currently in ${homePath}`);
-  };
-  getStartingPath();
-};
+  printCurrentPath() {
+    console.log(`You are currently in ${this.currentPath}`);
+  }
+}
