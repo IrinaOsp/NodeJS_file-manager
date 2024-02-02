9b5d0c10895f9af1033b0fbedf95e3ff4b48780c
Irina <108676588+IrinaOsp@users.noreply.github.com>
2024-02-02 13:23:10 +0100

    feat: add exit commands

--- src/modules/fileManager.js
+++ src/modules/fileManager.js
@@ -1,4 +1,5 @@
 import os from "os";
+import readline from "readline";
 
 export default class FileManager {
   constructor(username) {
@@ -9,9 +10,33 @@ export default class FileManager {
   startProject() {
     console.log(`Welcome to the File Manager, ${this.username}!`);
     this.printCurrentPath();
+    this.handleUserInput();
+  }
+
+  handleUserInput() {
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+
+    rl.on("line", (input) => {
+      console.log(`You entered: ${input}`);
+      if (input === ".exit") {
+        rl.close();
+      }
+    });
+
+    rl.on("close", () => {
+      this.onProgramExit();
+    });
   }
 
   printCurrentPath() {
     console.log(`You are currently in ${this.currentPath}`);
+  }
+
+  onProgramExit() {
+    console.log(`Thank you for using File Manager, ${this.username}, goodbye!`);
+    process.exit(0);
   }
 }
